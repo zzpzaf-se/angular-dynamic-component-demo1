93a33024ffbf249e28094f1200d752c4c89b33ea
Panos Zafeiropoulos <zzpzaf@gmail.com>
2023-10-31 17:40:26 +0200

    4th Commit Make the dynamic component agnostic to the host component. Use it within helper directive.

--- src/app/ad-dyn.directive.ts
+++ src/app/ad-dyn.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ViewContainerRef } from '@angular/core';
+import { Directive, Type, ViewContainerRef } from '@angular/core';
+import { MyDynamicComponent } from './my-dynamic/my-dynamic.component';
 
 @Directive({
   selector: '[adDynamic]'
@@ -6,5 +7,13 @@ import { Directive, ViewContainerRef } from '@angular/core';
 export class AdDynDirective {
 
   constructor(public viewContainerRef: ViewContainerRef) { }
+
+  createMyDynamicComponent() {
+    return this.viewContainerRef.createComponent(MyDynamicComponent);
+  }
+
+  // createMyDynamicComponent<C>(componentType: Type<C>) {
+  //   return this.viewContainerRef.createComponent(componentType);
+  // }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ComponentRef, OnInit, ViewChild, ViewContainerRef } from '@a
 import { MyDynamicComponent } from './my-dynamic/my-dynamic.component';
 import { AdDynDirective } from './ad-dyn.directive';
 
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -9,31 +10,38 @@ import { AdDynDirective } from './ad-dyn.directive';
 })
 export class AppComponent implements OnInit{
 
+
   public title: string = 'Dynamic Component Demo';
 
-  // @ViewChild('dynamic', { read: ViewContainerRef })
-  @ViewChild(AdDynDirective,{static: true}) adDynamic!: AdDynDirective 
 
-  private viewRef!: ViewContainerRef;
+  @ViewChild(AdDynDirective,{static: true}) 
+  adDynamic!: AdDynDirective; 
+  
+
   private componentRef!: ComponentRef<any>;
+
 
   public msg:string ="";
 
+
   ngOnInit(): void {
     console.log(">===>> " + this.title);
-    this.viewRef = this.adDynamic.viewContainerRef;
-    this.viewRef.clear();
+    this.adDynamic.viewContainerRef.clear();
   }
 
+
   showDynamicComponent(): void {
-    this.viewRef.clear();
-    this.componentRef = this.viewRef.createComponent(MyDynamicComponent); 
+    this.adDynamic.viewContainerRef.clear();
+    // this.componentRef = this.viewRef.createComponent(MyDynamicComponent); 
+    this.componentRef = this.adDynamic.createMyDynamicComponent() 
+
     this.componentRef.instance.subTitle = "My most-liked rose";
     this.componentRef.instance.dynEevent.subscribe((msg: string) => this.msg = msg);
   }
 
+
   removeDynamicComponent(): void {
-    this.viewRef.clear();
+    this.adDynamic.viewContainerRef.clear();
     this.msg = "";
   }
 
